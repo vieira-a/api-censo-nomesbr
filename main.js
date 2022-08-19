ec8d776fda414ac3ad149b2835257e97340d28e3
Anderson Vieira <asvieira.br@gmail.com>
2022-08-19 11:36:17 -0300

    Cria estrutura base HTML

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Variáveis iniciais
  */
 
-const myName = "MARIA"
+let myName = "JAMILE"
 
 const url = `https://servicodados.ibge.gov.br/api/v2/censos/nomes/${myName}`
 
@@ -18,25 +18,44 @@ async function getMyName() {
     //     let decade = name.periodo.replaceAll("[", "").replace(",", "-")
     //     frequency = name.frequencia
     // }) 
-    
+
     let myChart = new Chart(document.getElementById("my-chart"), {
         type: 'line',
         data: {
             datasets: [{
+                label: `Frequência do nome ${myName}`,
                 data: {
-                    "1930": dataJson[0].frequencia,
-                    "1930-1940": dataJson[1].frequencia,
-                    "1940-1950": dataJson[2].frequencia,
-                    "1950-1960": dataJson[3].frequencia,
-                    "1960-1970": dataJson[4].frequencia,
-                    "1970-1980": dataJson[5].frequencia,
-                    "1980-1990": dataJson[6].frequencia,
-                    "1990-2000": dataJson[7].frequencia,
-                    "2000-2010": dataJson[8].frequencia
-            }
+                    "1930": dataJson[1].frequencia,
+                    "1940": dataJson[2].frequencia,
+                    "1950": dataJson[3].frequencia,
+                    "1960": dataJson[4].frequencia,
+                    "1970": dataJson[5].frequencia,
+                    "1980": dataJson[6].frequencia,
+                    "1990": dataJson[7].frequencia,
+                    "2000": dataJson[8].frequencia
+            },
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: ['rgba(255,99,132,1)'],
+            borderWidth: 1
             }],
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            fontSize: 10
+                        }
+                    }]
+                }
+            }
         } 
     })
 
-}
+        }
+
+
 getMyName()
